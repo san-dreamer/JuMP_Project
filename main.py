f997e1941be10f5147d9cb5fde8f3ba15692ff13
san-dreamer <aimysandrashaji@gmail.com>
2025-04-28 10:25:08 +0400

    LLM on terminal

--- main.py
+++ main.py
@@ -1,4 +1,32 @@
-print('hello!')
-print('hello world!')
-print('hello world!')           
-print('hello world!')
+from langchain_ollama import OllamaLLM
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_community.llms import Ollama
+
+template = """
+Answer the questions below.
+Here is the conversation history: {context}
+Question: {question}
+Answer:
+"""
+
+model =  OllamaLLM(model="llama3")
+prompt = ChatPromptTemplate.from_template(template)
+chain = prompt | model
+
+def handle_conversation():
+    context = ""
+    print("Welcome, I am your AI Psychologist!")
+    while True:
+        question = input("\nAsk a question (or type 'exit'): ")
+        if question.lower() == "exit":
+            break
+
+        result = chain.invoke({"context": context, "question": question})
+        print("\nAnswer:", result)
+        context += f"User: {question}\nAssistant: {result}\n"
+
+if __name__ == "__main__":
+    handle_conversation()   
+
+result = chain.invoke({"context": "", "question": "hey, how are you?"})
+print(result)
